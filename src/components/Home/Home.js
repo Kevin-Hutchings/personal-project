94c8452c5da296af58fd616c01cb99436a061fac
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-11-27 11:45:57 -0700

    home component now has stuff

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { homeData } from "./data";
 import "../../css/components/Home.css";
+
+const dataMap = homeData.map((el, index) => {
+  const { title, content, content2, content3 } = el;
+  return (
+    <div className="home-info">
+      <h2 key={index}>{title}</h2>
+      <p>{content}</p>
+      <p>{content2}</p>
+      <p>{content3}</p>
+    </div>
+  );
+});
 
 const Home = () => {
   return (
-    <header>
-      <h1> A Love Letter to Studio Ghibli </h1>
-    </header>
+    <div className="home">
+      <header>
+        <h1> A Love Letter to Studio Ghibli </h1>
+      </header>
+      <div className="info-container">{dataMap}</div>
+    </div>
   );
 };
 
